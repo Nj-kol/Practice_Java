f755e26c6c6d79e1f5022fd814d9ab8003a37aef
Nj-Kol <nilanjan.sarkar100@gmail.com>
2021-12-15 08:46:53 +0530

    latest

--- java-common-techniques-demo/src/main/java/com/njkol/techniques/TechniquesDemoClient.java
+++ java-common-techniques-demo/src/main/java/com/njkol/techniques/TechniquesDemoClient.java
@@ -1,10 +1,14 @@
 package com.njkol.techniques;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class TechniquesDemoClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	
+      
 	}
 
 }
